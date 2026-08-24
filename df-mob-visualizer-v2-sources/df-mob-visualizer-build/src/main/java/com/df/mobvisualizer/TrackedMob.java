a9eb4bda3ecba231d40060b63ec996c012a62134
Darkf1f <kuprijegor15@gmail.com>
2026-08-24 03:42:32 +0300

    Add files via upload

---
+++ df-mob-visualizer-v2-sources/df-mob-visualizer-build/src/main/java/com/df/mobvisualizer/TrackedMob.java
@@ -0,0 +1,24 @@
+package com.df.mobvisualizer;
+
+public record TrackedMob(
+        int id,
+        String type,
+        String name,
+        int x,
+        int y,
+        int z,
+        boolean alert,
+        int color,
+        boolean player,
+        boolean hostile,
+        boolean hurt,
+        boolean chargedCreeper
+) {
+    public int chunkX() {
+        return x >> 4;
+    }
+
+    public int chunkZ() {
+        return z >> 4;
+    }
+}
